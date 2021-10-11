8f08e3b067bdf67ee700bf2ae105dbc37fad228b
Satya Bhan <sbhan@attentivemobile.com>
2021-10-11 15:25:24 -0400

    moar triggers

--- public/activity.js
+++ public/activity.js
@@ -2,10 +2,12 @@ var connection = new Postmonger.Session();
 
 // Startup Sequence
 connection.trigger('ready');
+connection.trigger('requestTokens');
+connection.trigger('requestTriggerEventDefinition');
 
 connection.on('initActivity', function( data ) {
   alert("initActivity");
-
+  console.log({data});
   document.getElementById( 'configuration' ).value = JSON.stringify( data, null, 2 );
 });
 
@@ -14,10 +16,15 @@ connection.on('requestedTriggerEventDefinition',
     alert("requestedTriggerEventDefinition");
       if (eventDefinitionModel) {
         console.log({eventDefinitionModel});
-        eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
+        //eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
       }
     });
 
+connection.on('requestedTokens',
+    function onGetTokens (tokens) {
+      // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
+      console.log({tokens});
+    });
 // Save Sequence
 connection.on('clickedNext', function() {
   var configuration = JSON.parse( document.getElementById( 'configuration' ).value );
