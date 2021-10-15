a2bf69226c2dddfa0a9e270627e954e14f6b4241
Satya Bhan <sbhan@attentivemobile.com>
2021-10-14 23:09:54 -0400

    test route

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ var app = express()
 var port = process.env.PORT || 3000
 
 app.use( express.static('public') )
-
+app.get('/test', (req, res) => {
+    res.send('Hello World!')
+})
 app.listen( port, () => console.log( `App listening on port ${port}!`) )
