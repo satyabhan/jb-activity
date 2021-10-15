e91600d971eb0c66dd36f9833c05ac267ed19cf4
Satya Bhan <sbhan@attentivemobile.com>
2021-10-14 23:18:19 -0400

    axios test

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require( 'express' )
 var app = express()
 var port = process.env.PORT || 3000
+const axios = require('axios');
 
 app.get('/test', (req, res) => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+        .then(response => {
+            console.log(response.data.url);
+            console.log(response.data.explanation);
+        })
+        .catch(error => {
+            console.log(error);
+        });
     res.send('Hello World!')
 })
 
